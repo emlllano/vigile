9eeb5e589e7f639fc7ef283ba0efc078984304a3
Emiliano Bustamante <119488315+emlllano@users.noreply.github.com>
2025-09-17 14:14:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,6 @@ sudo cp ~/vigile/osCustom/wallpapers/Lines.png /usr/share/wallpapers/
 sudo cp ~/vigile/osCustom/logo/logo.txt /usr/share/userlogo/
 sudo cp ~/vigile/osCustom/logo/matrix.jpg /usr/share/userlogo/
 sudo systemctl enable bluetooth.service
-sudo systemctl enable iwd.service
-sudo systemctl disable systemd-networkd-wait-online.service
-sudo systemctl mask systemd-networkd-wait-online.service
 mv ~/vigile/makeup/* ~/
 chsh -S /bin/fish
 source ~/vigile/nvidia.sh
