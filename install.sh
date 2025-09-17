c2a667c9b7af55ac09d6b5f537f07b089a5b17fc
Emiliano Bustamante <119488315+emlllano@users.noreply.github.com>
2025-09-17 14:13:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ echo packages were installed.
 sleep 3
 sudo cp ~/vigile/osCustom/os-release /etc/
 sudo mkdir /usr/share/wallpapers/ && sudo mkdir /usr/share/userlogo/
-sudo mv ~/vigile/osCustom/wallpapers/Lines.png /usr/share/wallpapers/
-sudo mv ~/vigile/osCustom/logo/logo.txt /usr/share/userlogo/
-sudo mv ~/vigile/osCustom/logo/matrix.jpg /usr/share/userlogo/
+sudo cp ~/vigile/osCustom/wallpapers/Lines.png /usr/share/wallpapers/
+sudo cp ~/vigile/osCustom/logo/logo.txt /usr/share/userlogo/
+sudo cp ~/vigile/osCustom/logo/matrix.jpg /usr/share/userlogo/
 sudo systemctl enable bluetooth.service
 sudo systemctl enable iwd.service
 sudo systemctl disable systemd-networkd-wait-online.service
