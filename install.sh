1202e176a86adac84ed49720da37f0cfec7204e0
Emiliano Bustamante <119488315+emlllano@users.noreply.github.com>
2025-09-17 14:03:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ source ~/vigile/packages.sh
 echo packages were installed.
 sleep 3
 sudo cp ~/vigile/osCustom/os-release /etc/
-sudo mkdir /usr/share/wallpapers/ && mkdir /usr/share/userlogo/
+sudo mkdir /usr/share/wallpapers/ && sudo mkdir /usr/share/userlogo/
 sudo mv ~/vigile/osCustom/wallpapers/Lines.png /usr/share/wallpapers/
 sudo mv ~/vigile/osCustom/logo/logo.txt /usr/share/userlogo/
 sudo mv ~/vigile/osCustom/logo/matrix.jpg /usr/share/userlogo/
