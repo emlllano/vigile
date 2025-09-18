4486df626eeee4bbad828b32eb897c48e454d4b8
Emiliano Bustamante <119488315+emlllano@users.noreply.github.com>
2025-09-18 12:50:00 +0200

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -4,9 +4,9 @@ sudo sed -i -e '/#\[multilib\]/,+1s/^#//' /etc/pacman.conf && \
 sudo pacman -Sy --noconfirm && \
 sudo pacman -S --noconfirm --needed linux-headers git base-devel tmux vim wget curl python lib32-zlib && \
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && \
-yay -S --noconfirm hyprland nwg-displays gtk-layer-shell hyprpolkitagent waybar hyprshot hyprutils xdg-desktop-portal-hyprland hyprpaper && \
+yay -S --noconfirm hyprland gtk-layer-shell hyprpolkitagent waybar hyprshot hyprutils xdg-desktop-portal-hyprland hyprpaper && \
 yay -S --noconfirm wofi hyprlang waybar hyprland-qtutils hyprland-protocols hypridle hyprgraphics && \
-yay -S --noconfirm swaync sushi 7zip rar obsidian capitaine-cursors nautilus errands gnome-calendar arch-audit cava kitty && \
+yay -S --noconfirm swaync sushi 7zip grim rar obsidian capitaine-cursors nautilus errands gnome-calendar arch-audit cava kitty && \
 yay -S --noconfirm ttf-nerd-fonts-symbols ttf-jetbrains-mono-nerd signal-desktop discord vlc ttf-firacode-nerd starship && \
 curl -fsS https://dl.brave.com/install.sh | sh && \
 yay -S --noconfirm pavucontrol-qt fish ranger && \
