d8977e8f9a2c26996274092c90d79a0f9684fd4d
Emiliano Bustamante <119488315+emlllano@users.noreply.github.com>
2025-09-22 10:41:35 +0200

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# ==============================================================================
-# Hyprland NVIDIA Setup Script for Arch Linux
-# ==============================================================================
-# This script automates the installation and configuration of NVIDIA drivers
-# for use with Hyprland on Arch Linux, following the official Hyprland wiki.
-#
-# Author: https://github.com/Kn0ax
-#
-# ==============================================================================
-
-# --- GPU Detection ---
-if [ -n "$(lspci | grep -i 'nvidia')" ]; then
-  # --- Driver Selection ---
-  # Turing (16xx, 20xx), Ampere (30xx), Ada (40xx), and newer recommend the open-source kernel modules
-  if echo "$(lspci | grep -i 'nvidia')" | grep -q -E "RTX [2-9][0-9]|GTX 16"; then
-    NVIDIA_DRIVER_PACKAGE="nvidia-open-dkms"
-  else
-    NVIDIA_DRIVER_PACKAGE="nvidia-dkms"
-  fi
-
   # Check which kernel is installed and set appropriate headers package
   KERNEL_HEADERS="linux-headers" # Default
   if pacman -Q linux-zen &>/dev/null; then
@@ -41,7 +21,7 @@ if [ -n "$(lspci | grep -i 'nvidia')" ]; then
   # Install packages
   PACKAGES_TO_INSTALL=(
     "${KERNEL_HEADERS}"
-    "${NVIDIA_DRIVER_PACKAGE}"
+    "nvidia-open-dkms"
     "nvidia-utils"
     "lib32-nvidia-utils"
     "egl-wayland"
